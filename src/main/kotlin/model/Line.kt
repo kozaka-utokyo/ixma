0c7605129c7749bf4ae32e41e51f670226b666f0
iNoma <702sunnylife@gmail.com>
2023-09-06 14:58:30 +0900

    Pageモデルをver03仕様にした
    デモ用のタグ管理機能は削除
    本来はデモ用としてバージョンを分けて管理するべきだった

---
+++ src/main/kotlin/model/Line.kt
@@ -0,0 +1,13 @@
+package model
+
+data class Line(
+    val value:String
+){
+    fun edit(new_val:String){
+        //TODO
+    }
+    fun devidedStringsByLinks():List<String>{
+        // TODO
+        return listOf("")
+    }
+}
