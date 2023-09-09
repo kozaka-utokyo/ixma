b86477df0369c91020cca3accb5d0cb0e00fbf28
github boohAika <iwama-aika@g.ecc.u-tokyo.ac.jp>
2023-09-09 09:11:40 +0900

    画像添付

---
+++ src/main/kotlin/ixma/Main_duck.kt
@@ -0,0 +1,19 @@
+package ixma
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.singleWindowApplication
+
+fun main() = singleWindowApplication {
+    Image(
+        painter = painterResource("duck.jpg"),
+        contentDescription = "Sample",
+        modifier = Modifier.fillMaxSize()
+    )
+
+}
+
