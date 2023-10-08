36de7aa3beeaa9fff534e87fa46fac7f13305c70
boohAika <iwama-aika@g.ecc.u-tokyo.ac.jp>
2023-10-08 20:25:25 +0900

    お気に入りの追加をおこないました。

--- src/main/kotlin/InitialScreen.kt
+++ src/main/kotlin/InitialScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -16,12 +17,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.material.TextField
+import androidx.compose.ui.input.key.*
 import ui.WindowController
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.awt.event.KeyEvent
 
 @Composable
 fun InitialScreen(windowController: WindowController) {
@@ -33,12 +38,9 @@ fun InitialScreen(windowController: WindowController) {
     val positions = remember { mutableStateListOf<Offset>() }
     val density = LocalDensity.current
 
-    val ahiruchan : String = "最近のプロジェクトの進行はいかがですか。"
+    val ahiruchan: String = "最近のプロジェクトの進行はいかがですか。"
     val ahiruchan_2: String = "気になるものがあれば押してね。"
     val ahiruchan_3: String = "研究頑張ろう。"
-
-
-
 
 
     val messageToShow = remember { mutableStateOf(ahiruchan) }
@@ -140,7 +142,7 @@ fun InitialScreen(windowController: WindowController) {
                             fontSize = 16.sp
                         )
                         LazyColumn(modifier = Modifier.weight(1f)) {
-                            val dummy_okiniiri = listOf("タンパク質A", "タンパク質B", "DNA-1", "mouse","PCR")
+                            val dummy_okiniiri = listOf("タンパク質A", "タンパク質B", "DNA-1", "mouse", "PCR")
                             items(dummy_okiniiri) { page ->
                                 Text(
                                     text = page,
@@ -157,8 +159,18 @@ fun InitialScreen(windowController: WindowController) {
                                 )
 
                             }
+
                         }
+                        val state = manageTextAndListState()
+
+                        TextBoxUI(state.textState, state.updateTextState, state.addText)
+
+                        state.stringList.forEach { item ->
+                            Text(item)
+                        }
+
                     }
+
 
                     Column() {
 
@@ -169,7 +181,15 @@ fun InitialScreen(windowController: WindowController) {
                             fontSize = 16.sp
                         )
                         LazyColumn(modifier = Modifier.weight(1f)) {
-                            val dummy_hizuke = listOf("2023-09-11(月)", "2023-09-12(火)", "2023-09-13(水)", "2023-09-14(木)","2023-09-15(金)","2023-09-16(土)","2023-09-17(日)")
+                            val dummy_hizuke = listOf(
+                                "2023-09-11(月)",
+                                "2023-09-12(火)",
+                                "2023-09-13(水)",
+                                "2023-09-14(木)",
+                                "2023-09-15(金)",
+                                "2023-09-16(土)",
+                                "2023-09-17(日)"
+                            )
                             items(dummy_hizuke) { page ->
                                 Text(
                                     text = page,
@@ -249,6 +269,85 @@ fun InitialScreen(windowController: WindowController) {
 
     }
 
+
 }
+
+data class TextAndListState(
+    val textState: TextFieldValue,
+    val stringList: List<String>,
+    val updateTextState: (TextFieldValue) -> Unit,
+    val addText: () -> Unit
+)
+
+
+@Composable
+fun manageTextAndListState(): TextAndListState {
+
+    var textState by remember { mutableStateOf(TextFieldValue()) }
+    var stringList by remember { mutableStateOf(listOf<String>()) }
+
+    fun updateTextState(newValue: TextFieldValue) {
+        textState = newValue
+    }
+
+    fun addText() {
+        stringList = stringList + textState.text
+        textState = TextFieldValue()
+    }
+    return TextAndListState(textState, stringList, ::updateTextState, ::addText)
+}
+
+
+
+
+
+@Composable
+fun TextBoxUI(
+    textState: TextFieldValue,
+    onTextChange: (TextFieldValue) -> Unit,
+    onAddButtonClick: () -> Unit
+) {
+    Column {
+        TextField(
+            textState,
+            onValueChange = onTextChange,
+            label = { Text("テキストを入力してください") },
+            modifier = Modifier.onKeyEvent {
+                event ->
+                if (event.key == Key(0x0a) ) { // 0x0A is the keycode for Enter
+                    print("Hello")
+                    onAddButtonClick()
+                    true
+
+                } else {
+                    println("--------")
+                    println(event.key)
+                    println(event.type)
+                    false
+
+
+                }
+            }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = onAddButtonClick) {
+            Text("追加")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
